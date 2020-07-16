1bba3b98926aad901e4c2ae105213dcefb57d977
Justin Kunimune <justinkunimune@gmail.com>
2020-07-16 12:06:03 -0400

    Better?
    I came up with a combination of electric fields that works better, but still doesn't quite capture the slopey quality of what I observe.

--- electric_field_model.py
+++ electric_field_model.py
@@ -2,12 +2,14 @@ import numpy as np
 
 
 R = np.linspace(0, 2, 1000) # normalized unbent position
-E = np.geomspace(1e0, 1e4, 40)
+E = np.geomspace(1e-1, 1e4, 50)
 K = 1/E
 N = np.empty((len(K), len(R)))
 for i, k in enumerate(K):
 	rS = np.concatenate([np.linspace(0, .9, 36)[:-1], (1 - np.geomspace(.1, 1e-6, 36))])
-	rB = rS + k*np.log((1 + 1/(1 - rS)**2)/(1 + 1/(1 + rS)**2))
+	rB = rS + k*(
+		np.log((1 + 1/(1 - rS)**2)/(1 + 1/(1 + rS)**2)) +\
+		np.log((1 + 1/(1 - rS/2)**2)/(1 + 1/(1 + rS/2)**2)))
 	nB = 1/(np.gradient(rB, rS)*rB/rS)
 	nB[rS > 1] = 0
 	nB[0] = nB[1] # deal with this singularity
@@ -16,7 +18,15 @@ N *= np.gradient(E)[:,np.newaxis] # normalize
 
 def get_analytic_brightness(r0, Q):
 	""" get the effective brightness as a function of radius, accounting for a roughly boxcar energy spectrum """
-	return r0*R, normalize(np.sum(N[K >= Q/r0, :], axis=0))
+	weight = 1 - 4*E/(r0/Q) + 4*(E/(r0/Q))**2
+	# weight = (E/(r0/Q))**2
+	# weight = np.ones(E.shape)
+	return r0*R, normalize(np.sum((N*weight[:,np.newaxis])[K >= Q/r0, :], axis=0))
 
 def normalize(x):
 	return x/x.max()
+
+if __name__ == '__main__':
+	import matplotlib.pyplot as plt
+	plt.plot(*get_analytic_brightness(1.5, 1e-3))
+	plt.show()
