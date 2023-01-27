d320b43acdfbdda2b476406bf6d3104dc3409070
Justin Kunimune <justinkunimune@gmail.com>
2023-01-27 09:58:26 -0500

    storming whitespace separated values

--- src/cmap.py
+++ src/cmap.py
@@ -8,6 +8,9 @@ CMAP: dict[str, ListedColormap] = {}
 directory = "data/tables" if os.path.isdir("data/tables") else "../data/tables"
 for filename in os.listdir(directory):
 	if filename.startswith("cmap_") and filename.endswith(".csv"):
-		cmap_data = np.loadtxt(os.path.join(directory, filename), delimiter=",")
+		try:
+			cmap_data = np.loadtxt(os.path.join(directory, filename))
+		except ValueError:
+			cmap_data = np.loadtxt(os.path.join(directory, filename), delimiter=",")
 		cmap_name = filename[5:-4]
 		CMAP[cmap_name] = ListedColormap(cmap_data, name=cmap_name)
