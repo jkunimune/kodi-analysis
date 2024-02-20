8bbf9a4e0b1659fcb2a3580c93ed2af93b867681
Justin Kunimune <justinkunimune@gmail.com>
2024-02-20 15:46:24 -0500

    force images to be in the source region
    I was having problems because the gaussian I was generating from the prior would sometimes be way off in the corner.  this makes it so it has to be at least in the source region circle.

--- src/mcmc.py
+++ src/mcmc.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy import reshape, sqrt, shape, exp, inf, pi, ones
 from numpy.typing import NDArray
-from pymc import Model, Gamma, Uniform, Truncated, sample, Normal, draw, TruncatedNormal
+from pymc import Model, Gamma, Uniform, Truncated, sample, Normal, draw, TruncatedNormal, Deterministic, Beta
 
 from coordinate import Image
 from util import standard_deviation
@@ -31,29 +31,43 @@ def deconvolve(data: Image, kernel: NDArray[float], guess: Image,
 		raise ValueError("these arrays don't have the right sizes")
 
 	with Model():
-		size = Gamma("size", mu=30, sigma=20, initval=standard_deviation(guess))
+		size = Gamma("size", mu=30e-4, sigma=20e-4, initval=standard_deviation(guess))
 		intensity_gess = np.sum(guess.values)*guess.domain.pixel_area/(2*pi*size**2)
 		intensity = Gamma("intensity", mu=intensity_gess, sigma=sqrt(2)*intensity_gess)
 		smoothness = 1#LogNormal("smoothness", mu=log(.1), sigma=2)
-		x0 = Uniform("x0", lower=0, upper=shape(source_region)[0])
-		y0 = Uniform("y0", lower=0, upper=shape(source_region)[1])
+		x0_normalized = 2*Beta.dist(alpha=3/2, beta=3/2) - 1
+		y0_normalized = Uniform.dist(lower=-sqrt(1 - x0_normalized**2), upper=sqrt(1 - x0_normalized**2))
+		x0 = Deterministic("x0", x0_normalized*guess.x.half_range + guess.x.center)
+		y0 = Deterministic("y0", y0_normalized*guess.y.half_range + guess.y.center)
 		X, Y = guess.domain.get_pixels(sparse=True)
 		gaussian = intensity*exp(-((X - x0)**2 + (Y - y0)**2)/(2*size**2))
-		source = TruncatedNormal("source",
-			mu=1, sigma=1/2,
-			# mu=gaussian,
-			# tau=-2/gaussian*eye(guess.num_pixels).reshape(source_region.shape*2) +
-			#     smoothness*discrete_laplacian(guess.domain),
-			lower=0, upper=inf,
-			shape=guess.shape,
-			# initval=guess.values,
-		)*gaussian
+		source = Deterministic(
+			"source",
+			gaussian*TruncatedNormal.dist(
+				mu=1, sigma=1/2,
+				# mu=gaussian,
+				# tau=-2/gaussian*eye(guess.num_pixels).reshape(source_region.shape*2) +
+				#     smoothness*discrete_laplacian(guess.domain),
+				lower=0, upper=inf,
+				shape=guess.shape,
+				# initval=guess.values,
+			),
+		)
 		print(draw(source))
 
 		inference = sample(1000)
 
 	arviz.plot_trace(inference, var_names=["size", "x0", "y0"])
-	plt.show()
+
+	if np.any(np.all(inference.posterior["source"].to_numpy() <= 0, axis=(2, 3))):
+		i, j = np.nonzero(np.all(inference.posterior["source"].to_numpy() == 0, axis=(2, 3)))
+		print("blank source alert!")
+		print(inference.posterior["size"].to_numpy()[i, j])
+		print(inference.posterior["x0"].to_numpy()[i, j])
+		print(inference.posterior["y0"].to_numpy()[i, j])
+		plt.figure()
+		plt.imshow(inference.posterior["source"].to_numpy()[i[0], j[0]], extent=guess.domain.extent)
+		plt.show()
 
 	return Image(
 		guess.domain,
