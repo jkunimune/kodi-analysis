fix background scaling
I think this may have been causing the divergences earlier.  but this is what I (God) intended when I defined the background parameter.  hopefully this is correct because I am *not* waiting for the code to run to push this change.

src/mcmc.py
@@ -61,9 +61,14 @@ def deconvolve(data: Image, kernel: NDArray[float], guess: Image,
 		source = Gamma("source", mu=intensity, sigma=sqrt(2)*intensity, shape=guess.shape, initval=np.maximum(np.max(guess.values)*.01, guess.values))
 		# source = Deterministic("source", base_shape*shape_modifier)
 		background = Gamma("background", mu=1/10, sigma=sqrt(2)/10)
-		true_image = pixel_area.values*(intensity*background + conv.conv2d(
-			tensor.shape_padleft(source, 2), tensor.shape_padleft(kernel, 2),
-			border_mode="full")[0, 0, :, :])
+		true_image = pixel_area.values*(
+			np.sum(guess.values)*np.max(kernel)*background + 
+			conv.conv2d(
+				tensor.shape_padleft(source, 2),
+				tensor.shape_padleft(kernel, 2),
+				border_mode="full"
+			)[0, 0, :, :]
+		)
 		if noise == "poisson":
 			image = Poisson("image", mu=true_image, observed=data.values)
 		else: