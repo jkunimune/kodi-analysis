81d76b8d6f3292f25aee24a8082da720678e47c3
Justin Kunimune <justinkunimune@gmail.com>
2024-03-07 11:23:41 -0500

    Fourier deconvolution is faster (sometimes)
    I need to benchmark this real quick to verify whether this was actually a help.

--- src/mcmc.py
+++ src/mcmc.py
@@ -1,15 +1,16 @@
-from typing import Union
 from multiprocessing import cpu_count
-import arviz
+from typing import Union
 
+import arviz
 import numpy as np
+import numpy.fft as np_fft
+import pytensor.tensor.fft as tensor_fft
 from matplotlib import pyplot as plt
-from numpy import reshape, sqrt, shape, pi
+from numpy import reshape, sqrt, shape, pi, real, imag, expand_dims
 from numpy.typing import NDArray
-from pymc import Model, Gamma, sample, Poisson, Normal
+from pymc import Model, Gamma, sample, Poisson, Normal, Deterministic
 from pymc.distributions.dist_math import check_parameters
 from pytensor import tensor
-from pytensor.tensor import conv
 
 from coordinate import Image
 from util import standard_deviation
@@ -34,11 +35,31 @@ def deconvolve(data: Image, kernel: NDArray[float], guess: Image,
 			data.shape[1] != guess.shape[1] + kernel.shape[1] - 1:
 		raise ValueError("these arrays don't have the right sizes")
 
+	# add a dummy "batch" dimension to all of these so that the archaic pytensor fft functions work
+	guess = Image(guess.domain, expand_dims(guess.values, axis=0))
+	kernel = expand_dims(kernel, axis=0)
+	data = Image(data.domain, expand_dims(data.values, axis=0))
+
+	# bring the inputs into the frequency domain
+	# data_spectrum = np_fft.rfft2(data.values)
+	kernel_spectrum = np_fft.rfft2(np.pad(
+		kernel,
+		((0, 0), (0, data.shape[1] - kernel.shape[1]), (0, data.shape[2] - kernel.shape[2])),
+	))
+	# guess_spectrum = np_fft.rfft2(np.pad(
+	# 	guess.values,
+	# 	((0, 0), (0, data.shape[0] - guess.shape[0]), (0, data.shape[1] - guess.shape[1])),
+	# ))
+	# convert numpy's complex numbers to arrays of real numbers, as pytensor prefers
+	# data_spectrum = np.stack([real(data_spectrum), imag(data_spectrum)], axis=-1)
+	kernel_spectrum = np.stack([real(kernel_spectrum), imag(kernel_spectrum)], axis=-1)
+	# guess_spectrum = np.stack([real(guess_spectrum), imag(guess_spectrum)], axis=-1)
+	image_intensity = np.sum(guess.values)*np.max(kernel)
+
 	with Model():
-		size = standard_deviation(guess)
+		size = standard_deviation(guess[0])
 		# size = Gamma("size", mu=30e-4, sigma=20e-4, initval=standard_deviation(guess))
-		intensity_gess = np.sum(guess.values)*guess.domain.pixel_area/(2*pi*size**2)
-		intensity = intensity_gess
+		source_intensity = np.sum(guess.values)*guess.domain.pixel_area/(2*pi*size**2)
 		# intensity = Gamma("intensity", mu=intensity_gess, sigma=sqrt(2)*intensity_gess)
 		# # intensity = np.max(guess.values)
 		# x0_normalized = 2*Beta("x0_normalized", alpha=3/2, beta=3/2) - 1
@@ -58,16 +79,24 @@ def deconvolve(data: Image, kernel: NDArray[float], guess: Image,
 		# 	shape=guess.shape,
 		# 	initval=np.maximum(np.max(guess.values)*.01, guess.values),
 		# )
-		source = Gamma("source", mu=intensity, sigma=sqrt(2)*intensity, shape=guess.shape, initval=np.maximum(np.max(guess.values)*.01, guess.values))
+		source = Gamma("source", mu=source_intensity, sigma=sqrt(2)*source_intensity, shape=guess.shape, initval=np.maximum(np.max(guess.values)*.01, guess.values))
 		# source = Deterministic("source", base_shape*shape_modifier)
+		source_spectrum = Deterministic(
+			"source_spectrum",
+			tensor_fft.rfft(pad_with_zeros(source, guess.shape, data.shape)),
+		)
 		background = Gamma("background", mu=1/10, sigma=sqrt(2)/10)
-		true_image = pixel_area.values*(
-			np.sum(guess.values)*np.max(kernel)*background + 
-			conv.conv2d(
-				tensor.shape_padleft(source, 2),
-				tensor.shape_padleft(kernel, 2),
-				border_mode="full"
-			)[0, 0, :, :]
+
+		true_image_spectrum = Deterministic(
+			"true_image_spectrum", complex_multiply(source_spectrum, kernel_spectrum))
+		true_image = Deterministic(
+			"true_image",
+			pixel_area.values*(
+				image_intensity*background +
+				Deterministic("true_image_plain", tensor_fft.irfft(
+					true_image_spectrum, is_odd=data.shape[2]%2 == 1
+				))
+			),
 		)
 		if noise == "poisson":
 			image = Poisson("image", mu=true_image, observed=data.values)
@@ -101,7 +130,7 @@ def deconvolve(data: Image, kernel: NDArray[float], guess: Image,
 
 	return Image(
 		guess.domain,
-		reshape(inference.posterior["source"].to_numpy(), (-1, guess.shape[0], guess.shape[1]))
+		reshape(inference.posterior["source"].to_numpy(), (-1, guess.shape[1], guess.shape[2]))
 	)
 
 
@@ -116,3 +145,24 @@ def truncated_spacially_correlated_distribution_logp(values, mu, sigma, x_factor
 	x_penalties = (x_factor*(values[0:-1, :] - values[1:, :]))**2
 	y_penalties = (y_factor*(values[:, 0:-1] - values[:, 1:]))**2
 	return -(tensor.sum(identity_penalties) + tensor.sum(x_penalties) + tensor.sum(y_penalties))
+
+
+def complex_multiply(a, b):
+	""" treating two float tensors of shape (..., 2) as complex tensors of shape (...), multiply them elementwise. """
+	c_real = a[..., 0]*b[..., 0] - a[..., 1]*b[..., 1]
+	c_imag = a[..., 0]*b[..., 1] + a[..., 1]*b[..., 0]
+	return tensor.stack([c_real, c_imag], axis=-1)
+
+
+def pad_with_zeros(a, old_shape, new_shape):
+	""" this does more or less the same thing as numpy.pad() but for pytensor tensors """
+	for axis in range(len(new_shape)):
+		if new_shape[axis] < old_shape[axis]:
+			raise ValueError("the requested shape must be no bigger than the input tensor in any dimension")
+		elif new_shape[axis] == old_shape[axis]:
+			pass
+		else:
+			supplement_shape = new_shape[:axis] + (new_shape[axis] - old_shape[axis],) + old_shape[axis + 1:]
+			supplement = tensor.zeros(supplement_shape)
+			a = tensor.concatenate([a, supplement], axis=axis)
+	return a
